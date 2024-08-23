edb18a2111245e30e528a509636744f2589aedbd
FelipeFernandes206 <felipe.feeeernanes.s@gmail.com>
2024-08-23 12:10:18 -0300

    novo
     novo

---
+++ index.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    return a +b
+
+def subtracao(a, b):
+    return a - b 
+
+if _name_ == "_main_":
+    print(soma(10, 5))
+    print(subtracao(10, 5))
+def multiplicacao(a ,b):
+    return a * b
